1305f0cbc12de0c2c827fa15fe2b4f2f98a87316
kibobe <tina.mastilovic@gmail.com>
2017-06-08 11:10:35 +0200

    Add -h (help) option. Print available relations.

--- main.cpp
+++ main.cpp
@@ -15,8 +15,24 @@ int main(int argc, char* argv[])
 {
     bool optIndicator = false;
 
-    if(argc > 1 && strcmp(argv[1], "-o") == 0){
-        optIndicator = true;
+    for(int i = 1; i < argc ;i++){
+        
+	if(strcmp(argv[i], "-o") == 0){
+            optIndicator = true;
+            
+        }
+        
+        if(strcmp(argv[i], "-h") == 0){
+            cout <<  "USE: \n"
+                 <<  "collinear(A1, A2, A3) \n" 
+                 <<  "parallel(A1, A2, A3, A4) \n"
+                 <<  "perpendicular(A1, A2, A3, A4)  \n"
+                 <<  "lengths_eq(A1, A2, A3, A4)  \n"
+                 <<  "is_midA(A1, A2, A3)  \n"
+                 <<  "is_intersection(A1, A2, A3, A4, A5)  \n"
+                 <<  "is_equal(A2, A2) \n\n" << endl;
+        }
+        
     }
 
     while(true){
